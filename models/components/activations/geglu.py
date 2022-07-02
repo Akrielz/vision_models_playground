31ee35a8d0dde46553982eae9804985abc400006
Akrielz <akrielz@yahoo.com>
2022-07-02 14:04:34 +0300

    Added Quick Gelu

--- models/components/activations/geglu.py
+++ models/components/activations/geglu.py
@@ -3,9 +3,6 @@ from torch.nn import functional as F
 
 
 class GEGLU(nn.Module):
-    def __init__(self):
-        super(GEGLU, self).__init__()
-
     def forward(self, x):
         x, gates = x.chunk(2, dim=-1)
         return x * F.gelu(gates)
--- models/components/activations/relu_squared.py
+++ models/components/activations/relu_squared.py
@@ -4,8 +4,5 @@ import torch.nn.functional as F
 
 
 class ReluSquared(nn.Module):
-    def __init__(self):
-        super(ReluSquared, self).__init__()
-
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return F.relu(x) ** 2
