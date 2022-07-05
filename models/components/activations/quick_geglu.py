849d3a203cce251b36f15c7fa899dd37fd3e374b
Akrielz <akrielz@yahoo.com>
2022-07-05 14:10:13 +0300

    renamed var

--- models/components/activations/quick_geglu.py
+++ models/components/activations/quick_geglu.py
@@ -6,8 +6,8 @@ from models.components import QuickGELU
 class QuickGEGLU(nn.Module):
     def __init__(self):
         super(QuickGEGLU, self).__init__()
-        self.gelu = QuickGELU()
+        self.quick_gelu = QuickGELU()
 
     def forward(self, x):
         x, gates = x.chunk(2, dim=-1)
-        return x * self.gelu(gates)
+        return x * self.quick_gelu(gates)
