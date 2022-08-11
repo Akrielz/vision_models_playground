f11953a78d6806a58a4d7cb86bb3abceed2c1d99
Akrielz <akrielz@yahoo.com>
2022-08-11 19:35:55 +0300

    Added shortcut

--- components/attention/__init__.py
+++ components/attention/__init__.py
@@ -8,4 +8,5 @@ from components.attention.attend import Attend
 from components.attention.feed_forward import FeedForward
 from components.attention.transformer_decoder import TransformerDecoder
 from components.attention.compressor import Compressor
+from components.attention.transformer import Transformer
 from components.attention.transformer_encoder import TransformerEncoder
