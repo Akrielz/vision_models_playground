db0bcfe31b1de9f8a343a6902036b9b4a8bd6493
Akrielz <akrielz@yahoo.com>
2023-05-27 23:46:12 +0300

    added tied_embedding

---
+++ vision_models_playground/components/attention/tied_embedding.py
@@ -0,0 +1,42 @@
+import torch
+
+
+class TiedEmbedding(torch.nn.Module):
+
+    def __init__(self, vocab_size: int, embedding_size: int):
+        super(TiedEmbeddingSoftmax, self).__init__()
+
+        self.w = torch.nn.Parameter(torch.randn(vocab_size, embedding_size))
+        self.b = torch.nn.Parameter(torch.randn(vocab_size))
+
+    def forward(self, inputs: torch.Tensor, embed: bool = True):
+        if embed:
+            return torch.nn.functional.embedding(inputs, self.w)
+        else:
+            return torch.tensordot(inputs, self.w.t(), 1) + self.b
+
+
+def main():
+    for embedding_size in range(1, 50):
+
+        tied_embedding_softmax = TiedEmbedding(vocab_size, embedding_size)
+        tied_embedding_softmax.eval()
+
+        acc = 0
+
+        for t in range(100):
+            inputs = torch.tensor([i for i in range(vocab_size)])
+            embedded = tied_embedding_softmax(inputs, embed=True)
+
+            outputs_softmax = tied_embedding_softmax(embedded, embed=False)
+            outputs = torch.argmax(outputs_softmax, dim=1)
+
+            # check if the outputs are the same as the inputs
+            correct = torch.eq(inputs, outputs).sum() / len(inputs)
+            acc = acc + correct.item()
+
+        print(f"embedding_size: {embedding_size}, acc: {acc}")
+
+
+if __name__ == "__main__":
+    main()
